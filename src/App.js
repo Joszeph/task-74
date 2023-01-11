4d0702255d062e0576620ffee32e86ef16ef3017
Yosif Georgiev <iodesign@live.com>
2023-01-11 17:31:32 +0200

    NEW: add regex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useMemo, useState } from "react";
 import "./App.css";
 
 function App() {
+
+  const [text, setText] = useState('')
+
+  const isValidNumber = useMemo(()=>{
+    const numRegex = /^[0-9]*$/
+    return numRegex.test(text)
+  },[text])
+
   return (
     <div className="App">
      <div className="control has-icons-right">
@@ -8,10 +17,12 @@ function App() {
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          value={text}
+          onChange={(e)=>setText(e.target.value)}
         />
-        <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+        <span className="icon is-small is-right" >
+
+          <i className={`fas ${isValidNumber ? 'fa-check': 'fa-times'}`} />
         </span> 
       </div>
     </div>
